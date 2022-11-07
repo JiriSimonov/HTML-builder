d196ba22fb141cd80ce21ca9c64500a7e54da533
JiriSimonov <jirisimonov@gmail.com>
2022-11-07 19:07:23 +0300

    fix: solution for 06 task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,35 +1,30 @@
-let fs = require("fs");
-let path = require("path");
-
-const copyFolder = async () => {
-  fs.readdir(path.join(__dirname, "assets"), (err, folders) => {
+const fs = require("fs");
+const path = require("path");
+const inputAssets = path.join(__dirname, "assets");
+const outputAssets = path.join(__dirname, "project-dist", "assets");
+const copyFolder = () => {
+  fs.readdir(inputAssets, (err, folders) => {
     if (err) throw err;
-    fs.mkdir(
-      path.join(__dirname, "project-dist", "assets"),
-      { recursive: true },
-      (err) => {
-        for (const folder of folders) {
-          fs.stat(path.join(__dirname, "assets", folder), (err, stats) => {
+    fs.mkdir(outputAssets, { recursive: true }, (err) => {
+      if (err) throw err;
+      for (const folder of folders) {
+        fs.stat(path.join(inputAssets, folder), (err, stats) => {
+          if (err) throw err;
+          if (stats.isDirectory()) {
             fs.mkdir(
-              path.join(__dirname, "project-dist", "assets", folder),
+              path.join(outputAssets, folder),
               { recursive: true },
               (err) => {
                 if (err) throw err;
                 fs.readdir(
-                  path.join(__dirname, "assets", folder),
+                  path.join(inputAssets, folder),
                   { recursive: true },
                   (err, items) => {
                     if (err) throw err;
                     for (const item of items) {
                       fs.copyFile(
-                        path.join(__dirname, "assets", folder, item),
-                        path.join(
-                          __dirname,
-                          "project-dist",
-                          "assets",
-                          folder,
-                          item
-                        ),
+                        path.join(inputAssets, folder, item),
+                        path.join(outputAssets, folder, item),
                         (err) => {
                           if (err) throw err;
                         }
@@ -39,10 +34,18 @@ const copyFolder = async () => {
                 );
               }
             );
-          });
-        }
+          } else {
+            fs.copyFile(
+              path.join(inputAssets, folder),
+              path.join(outputAssets, folder),
+              (err) => {
+                if (err) throw err;
+              }
+            );
+          }
+        });
       }
-    );
+    });
   });
 };
 const bundleCss = () => {
@@ -61,7 +64,7 @@ const bundleCss = () => {
                 if (err) throw err;
                 let content = data + "\n";
                 fs.appendFile(
-                  path.join(__dirname, "project-dist", "styles.css"),
+                  path.join(__dirname, "project-dist", "style.css"),
                   content,
                   function (err) {
                     if (err) throw err;
@@ -76,6 +79,25 @@ const bundleCss = () => {
   });
 };
 
+const bundleHTML = () => {
+    fs.readFile(path.join(__dirname, 'template.html'), 'utf8', function (err, data) {
+        if (err) throw err;
+        let result = data;
+        const namesComponents = [...data.matchAll(/{{(.*)}}/g)].map((el) => el[1]);
+        namesComponents.forEach((component) => {
+            fs.readFile(path.join(__dirname, 'components', `${component}.html`), 'utf-8', function (err, data) {
+                if (err) throw err;
+                result = result.replace(`{{${component}}}`, data);
+                fs.writeFile(path.join(__dirname, 'project-dist', 'index.html'), result, function(err) {
+                    if (err) throw err;
+                });
+            });
+        });
+    });
+}
+
+
+
 (async () => {
   await fs.promises.rm(path.join(__dirname, "project-dist"), {
     recursive: true,
@@ -84,106 +106,7 @@ const bundleCss = () => {
   await fs.promises.mkdir(path.join(__dirname, "project-dist"), {
     recursive: true,
   });
-  await copyFolder();
-  await bundleCss();
+  bundleHTML();
+  copyFolder();
+  bundleCss();
 })();
-// fs.mkdir(path.join(__dirname, 'project-dist'), {recursive: true}, err => {
-//     if (err) throw err;
-//     fs.copyFile(path.join(__dirname, 'template.html'), path.join(__dirname, 'project-dist', 'index.html'), (err) => {
-//         if(err) throw err;
-//         fs.readdir(path.join(__dirname, "components"), (err, components) => {
-//             if (err) throw err;
-//             let articles = components[0];
-//             let footer = components[1];
-//             let header = components[2];
-//             fs.readFile(path.join(__dirname, "components", articles), 'utf-8', function(err, data) {
-//                 if (err) throw err;
-//                 let content = data;
-//                 fs.readFile(path.join(__dirname, 'project-dist', 'index.html'), 'utf-8', async function(err, data) {
-//                     if (err) {
-//                         if (err) throw err;
-//                     }
-//                    let result = data.replace(`{{${path.basename(articles).split('.')[0]}}}`, content);
-//                     fs.writeFile(path.join(__dirname, 'project-dist', 'index.html'), result, 'utf-8', function (err) {
-//                         if (err) throw err;
-//                     });
-//                 });
-//             });
-//             fs.readFile(path.join(__dirname, "components", articles), 'utf-8', function(err, data) {
-//                 if (err) throw err;
-//                 let content = data;
-//                 fs.readFile(path.join(__dirname, 'project-dist', 'index.html'), 'utf-8', function(err, data) {
-//                     if (err) {
-//                         if (err) throw err;
-//                     }
-//                    let result = data.replace(`{{${path.basename(articles).split('.')[0]}}}`, content);
-//                     fs.writeFile(path.join(__dirname, 'project-dist', 'index.html'), result, 'utf-8', function (err) {
-//                         if (err) throw err;
-//                     });
-//                     fs.readFile(path.join(__dirname, "components", footer), 'utf-8', function(err, data) {
-//                         if (err) throw err;
-//                         let content = data;
-//                         fs.readFile(path.join(__dirname, 'project-dist', 'index.html'), 'utf-8', function(err, data) {
-//                             if (err) {
-//                                 if (err) throw err;
-//                             }
-//                            let result = data.replace(`{{${path.basename(footer).split('.')[0]}}}`, content);
-//                             fs.writeFile(path.join(__dirname, 'project-dist', 'index.html'), result, 'utf-8', function (err) {
-//                                 if (err) throw err;
-//                             });
-//                             fs.readFile(path.join(__dirname, "components", header), 'utf-8', function(err, data) {
-//                                 if (err) throw err;
-//                                 let content = data;
-//                                 fs.readFile(path.join(__dirname, 'project-dist', 'index.html'), 'utf-8', function(err, data) {
-//                                     if (err) {
-//                                         if (err) throw err;
-//                                     }
-//                                    let result = data.replace(`{{${path.basename(header).split('.')[0]}}}`, content);
-//                                     fs.writeFile(path.join(__dirname, 'project-dist', 'index.html'), result, 'utf-8', function (err) {
-//                                         if (err) throw err;
-//                                     });
-//                                 });
-//                             });
-//                         });
-//                     });
-//                 });
-//             });
-//             fs.readdir(path.join(__dirname, "styles"), (err, files) => {
-//                 if (err) throw err;
-//                 for (const file of files) {
-//                     fs.stat(path.join(__dirname, "styles", file), (err, stats) => {
-//                         if (err) throw err;
-//                         if (path.extname(file) === '.css') {
-//                             fs.readFile(path.join(__dirname, "styles", file), 'utf-8', function(err, data) {
-//                                 let content = data + "\n";
-//                                 fs.appendFile(path.join(__dirname, "project-dist", 'style.css'), content, function(err) {
-//                                     if(err) throw err;
-//                                 });
-//                             });
-//                         }
-//                     });
-//                 }
-//             });
-//             fs.readdir(path.join(__dirname, 'assets'), (err, folders) => {
-//                 if (err) throw err;
-//                 fs.mkdir(path.join(__dirname, 'project-dist', 'assets'), {recursive: true}, err => {
-//                     for (const folder of folders) {
-//                         fs.stat(path.join(__dirname, "assets", folder), (err, stats) => {
-//                             fs.mkdir(path.join(__dirname, "project-dist", 'assets', folder), {recursive: true}, err => {
-//                                 if (err) throw err;
-//                                 fs.readdir(path.join(__dirname, "assets", folder), {recursive: true}, (err, items) => {
-//                                     if (err) throw err;
-//                                     for (const item of items) {
-//                                         fs.copyFile(path.join(__dirname, "assets", folder, item), path.join(__dirname, 'project-dist', 'assets', folder, item), err =>{
-//                                             if (err) throw err;
-//                                         });
-//                                     }
-//                                 });
-//                             });
-//                         });
-//                     }
-//                 })
-//             });
-//         });
-//     });
-// });
