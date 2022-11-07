605365c0c079aa9f3b5a18ddb37be40e1dda8e59
JiriSimonov <jirisimonov@gmail.com>
2022-11-07 15:46:58 +0300

    fix: solution for task 3

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -7,4 +7,4 @@ fs.readFile(path.join(__dirname, 'text.txt'), function (err, data) {
     }
     console.log(data.toString());
 })
-console.log(path.basename(__filename))
+console.log(path.basename(__filename));
--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,12 +1,18 @@
 let path = require('path');
 let fs = require('fs');
 
-fs.readdir(path.join(__dirname, "secret-folder"), (err, files) => {
-    if (err) throw err;   
+fs.readdir(path.join(__dirname, 'secret-folder'), (err, files) => {
+    if (err) throw err;
     for (const file of files) {
-        fs.stat(path.join(__dirname, "secret-folder", file), (err, stats) => {
+        fs.stat(path.join(__dirname, 'secret-folder', file), (err, stats) => {
             if (err) throw err;
-            console.log(`${path.basename(file).split('.')[0]} - ${path.extname(file).slice(1)} - ${stats.size}`);
+            if (stats.isFile()) {
+                console.log(
+                    `${path.basename(file).split('.')[0]} - ${path
+                        .extname(file)
+                        .slice(1)} - ${stats.size}`
+                );
+            }
         });
     }
-})
+});
