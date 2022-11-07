9e9ba1f8044251bdc73109fda649936c712c1c70
JiriSimonov <jirisimonov@gmail.com>
2022-11-07 15:36:21 +0300

    fix: task 2 solution

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,21 +1,25 @@
-const fs = require("fs");
+const fs = require('fs');
 const path = require('path');
 const { stdout, stdin } = process;
 
-stdout.write("Привет! Какой текст добавим? \n");
+stdout.write('Привет! Какой текст добавим? \n');
 
 function setText(text) {
-  fs.appendFile(path.join(__dirname, 'text.txt'), text, err => {
-    if (err) throw err;
-  });
+    if (text.toString().match('exit')) {
+        setExit();
+    } else {
+        fs.appendFile(path.join(__dirname, 'text.txt'), text, (err) => {
+            if (err) throw err;
+        });
+    }
 }
 
-function setExite() {
-  stdout.write("Пока!");
+function setExit() {
+    stdout.write('Пока!');
+    process.exit(0);
 }
 
-stdin.on("data", setText);
-process.on("SIGINT", code => {
-  console.log('До свидания!');
-  process.exit(0);
+stdin.on('data', setText);
+process.on('SIGINT', (code) => {
+    setExit();
 });
